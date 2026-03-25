81b342d8f685022a02e81bb24476474d62877230
arunkumar2891-ux <arunkumar2891@gmail.com>
2026-03-25 07:49:04 +0530

    Update api.ts

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,5 +1,5 @@
 const API_URL = import.meta.env.VITE_API_URL;
-console.log(import.meta.env.VITE_API_URL);
+//console.log(import.meta.env.VITE_API_URL);
 //console.log(`API_URL: ${API_URL}`);
 
 export const api = {
