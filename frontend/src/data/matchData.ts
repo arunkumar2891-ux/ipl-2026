b7d89158e9f18c9dfa4213f9040608bb693e6e32
arunkumar2891-ux <arunkumar2891@gmail.com>
2026-03-27 18:50:21 +0530

    Update matchData.ts
    Schedule till 70 Matches

--- frontend/src/data/matchData.ts
+++ frontend/src/data/matchData.ts
@@ -10,14 +10,6 @@ export interface MatchDataItem {
 
 export const matchData: MatchDataItem[] = [
   {
-    "MatchNumber": 0,
-    "DateUtc": "2026-03-25T10:00:00Z",
-    "RoundNumber": 1,
-    "HomeTeam": "Royal Challengers Bengaluru",
-    "AwayTeam": "Sunrisers Hyderabad",
-    "Location": "Bengaluru"
-  },
-  {
     "MatchNumber": 1,
     "DateUtc": "2026-03-28T14:00:00Z",
     "RoundNumber": 1,
@@ -28,7 +20,7 @@ export const matchData: MatchDataItem[] = [
   {
     "MatchNumber": 2,
     "DateUtc": "2026-03-29T14:00:00Z",
-    "RoundNumber": 1,
+    "RoundNumber": 2,
     "HomeTeam": "Mumbai Indians",
     "AwayTeam": "Kolkata Knight Riders",
     "Location": "Mumbai"
@@ -36,7 +28,7 @@ export const matchData: MatchDataItem[] = [
   {
     "MatchNumber": 3,
     "DateUtc": "2026-03-30T14:00:00Z",
-    "RoundNumber": 1,
+    "RoundNumber": 3,
     "HomeTeam": "Rajasthan Royals",
     "AwayTeam": "Chennai Super Kings",
     "Location": "Guwahati"
@@ -44,7 +36,7 @@ export const matchData: MatchDataItem[] = [
   {
     "MatchNumber": 4,
     "DateUtc": "2026-03-31T14:00:00Z",
-    "RoundNumber": 1,
+    "RoundNumber": 4,
     "HomeTeam": "Punjab Kings",
     "AwayTeam": "Gujarat Titans",
     "Location": "New Chandigarh"
@@ -52,7 +44,7 @@ export const matchData: MatchDataItem[] = [
   {
     "MatchNumber": 5,
     "DateUtc": "2026-04-01T14:00:00Z",
-    "RoundNumber": 1,
+    "RoundNumber": 5,
     "HomeTeam": "Lucknow Super Giants",
     "AwayTeam": "Delhi Capitals",
     "Location": "Lucknow"
@@ -60,7 +52,7 @@ export const matchData: MatchDataItem[] = [
   {
     "MatchNumber": 6,
     "DateUtc": "2026-04-02T14:00:00Z",
-    "RoundNumber": 1,
+    "RoundNumber": 6,
     "HomeTeam": "Kolkata Knight Riders",
     "AwayTeam": "Sunrisers Hyderabad",
     "Location": "Kolkata"
@@ -68,7 +60,7 @@ export const matchData: MatchDataItem[] = [
   {
     "MatchNumber": 7,
     "DateUtc": "2026-04-03T14:00:00Z",
-    "RoundNumber": 1,
+    "RoundNumber": 7,
     "HomeTeam": "Chennai Super Kings",
     "AwayTeam": "Punjab Kings",
     "Location": "Chennai"
@@ -76,7 +68,7 @@ export const matchData: MatchDataItem[] = [
   {
     "MatchNumber": 8,
     "DateUtc": "2026-04-04T10:00:00Z",
-    "RoundNumber": 1,
+    "RoundNumber": 8,
     "HomeTeam": "Delhi Capitals",
     "AwayTeam": "Mumbai Indians",
     "Location": "Delhi"
@@ -84,7 +76,7 @@ export const matchData: MatchDataItem[] = [
   {
     "MatchNumber": 9,
     "DateUtc": "2026-04-04T14:00:00Z",
-    "RoundNumber": 1,
+    "RoundNumber": 9,
     "HomeTeam": "Gujarat Titans",
     "AwayTeam": "Rajasthan Royals",
     "Location": "Ahmedabad"
@@ -92,7 +84,7 @@ export const matchData: MatchDataItem[] = [
   {
     "MatchNumber": 10,
     "DateUtc": "2026-04-05T10:00:00Z",
-    "RoundNumber": 1,
+    "RoundNumber": 10,
     "HomeTeam": "Sunrisers Hyderabad",
     "AwayTeam": "Lucknow Super Giants",
     "Location": "Hyderabad"
@@ -100,7 +92,7 @@ export const matchData: MatchDataItem[] = [
   {
     "MatchNumber": 11,
     "DateUtc": "2026-04-05T14:00:00Z",
-    "RoundNumber": 1,
+    "RoundNumber": 11,
     "HomeTeam": "Royal Challengers Bengaluru",
     "AwayTeam": "Chennai Super Kings",
     "Location": "Bengaluru"
@@ -108,7 +100,7 @@ export const matchData: MatchDataItem[] = [
   {
     "MatchNumber": 12,
     "DateUtc": "2026-04-06T14:00:00Z",
-    "RoundNumber": 1,
+    "RoundNumber": 12,
     "HomeTeam": "Kolkata Knight Riders",
     "AwayTeam": "Punjab Kings",
     "Location": "Kolkata"
@@ -116,7 +108,7 @@ export const matchData: MatchDataItem[] = [
   {
     "MatchNumber": 13,
     "DateUtc": "2026-04-07T14:00:00Z",
-    "RoundNumber": 1,
+    "RoundNumber": 13,
     "HomeTeam": "Rajasthan Royals",
     "AwayTeam": "Mumbai Indians",
     "Location": "Guwahati"
@@ -124,7 +116,7 @@ export const matchData: MatchDataItem[] = [
   {
     "MatchNumber": 14,
     "DateUtc": "2026-04-08T14:00:00Z",
-    "RoundNumber": 1,
+    "RoundNumber": 14,
     "HomeTeam": "Delhi Capitals",
     "AwayTeam": "Gujarat Titans",
     "Location": "Delhi"
@@ -132,7 +124,7 @@ export const matchData: MatchDataItem[] = [
   {
     "MatchNumber": 15,
     "DateUtc": "2026-04-09T14:00:00Z",
-    "RoundNumber": 1,
+    "RoundNumber": 15,
     "HomeTeam": "Kolkata Knight Riders",
     "AwayTeam": "Lucknow Super Giants",
     "Location": "Kolkata"
@@ -140,7 +132,7 @@ export const matchData: MatchDataItem[] = [
   {
     "MatchNumber": 16,
     "DateUtc": "2026-04-10T14:00:00Z",
-    "RoundNumber": 1,
+    "RoundNumber": 16,
     "HomeTeam": "Rajasthan Royals",
     "AwayTeam": "Royal Challengers Bengaluru",
     "Location": "Guwahati"
@@ -148,7 +140,7 @@ export const matchData: MatchDataItem[] = [
   {
     "MatchNumber": 17,
     "DateUtc": "2026-04-11T10:00:00Z",
-    "RoundNumber": 1,
+    "RoundNumber": 17,
     "HomeTeam": "Punjab Kings",
     "AwayTeam": "Sunrisers Hyderabad",
     "Location": "New Chandigarh"
@@ -156,7 +148,7 @@ export const matchData: MatchDataItem[] = [
   {
     "MatchNumber": 18,
     "DateUtc": "2026-04-11T14:00:00Z",
-    "RoundNumber": 1,
+    "RoundNumber": 18,
     "HomeTeam": "Chennai Super Kings",
     "AwayTeam": "Delhi Capitals",
     "Location": "Chennai"
@@ -164,7 +156,7 @@ export const matchData: MatchDataItem[] = [
   {
     "MatchNumber": 19,
     "DateUtc": "2026-04-12T10:00:00Z",
-    "RoundNumber": 1,
+    "RoundNumber": 19,
     "HomeTeam": "Lucknow Super Giants",
     "AwayTeam": "Gujarat Titans",
     "Location": "Lucknow"
@@ -172,10 +164,410 @@ export const matchData: MatchDataItem[] = [
   {
     "MatchNumber": 20,
     "DateUtc": "2026-04-12T14:00:00Z",
-    "RoundNumber": 1,
+    "RoundNumber": 20,
     "HomeTeam": "Mumbai Indians",
     "AwayTeam": "Royal Challengers Bengaluru",
     "Location": "Mumbai"
+  },
+  {
+    "MatchNumber": 21,
+    "DateUtc": "2026-04-13T14:00:00Z",
+    "RoundNumber": 21,
+    "HomeTeam": "Sunrisers Hyderabad",
+    "AwayTeam": "Rajasthan Royals",
+    "Location": "Hyderabad"
+  },
+  {
+    "MatchNumber": 22,
+    "DateUtc": "2026-04-14T14:00:00Z",
+    "RoundNumber": 22,
+    "HomeTeam": "Chennai Super Kings",
+    "AwayTeam": "Kolkata Knight Riders",
+    "Location": "Chennai"
+  },
+  {
+    "MatchNumber": 23,
+    "DateUtc": "2026-04-15T14:00:00Z",
+    "RoundNumber": 23,
+    "HomeTeam": "Royal Challengers Bengaluru",
+    "AwayTeam": "Lucknow Super Giants",
+    "Location": "Bengaluru"
+  },
+  {
+    "MatchNumber": 24,
+    "DateUtc": "2026-04-16T14:00:00Z",
+    "RoundNumber": 24,
+    "HomeTeam": "Mumbai Indians",
+    "AwayTeam": "Punjab Kings",
+    "Location": "Mumbai"
+  },
+  {
+    "MatchNumber": 25,
+    "DateUtc": "2026-04-17T14:00:00Z",
+    "RoundNumber": 25,
+    "HomeTeam": "Gujarat Titans",
+    "AwayTeam": "Kolkata Knight Riders",
+    "Location": "Ahmedabad"
+  },
+  {
+    "MatchNumber": 26,
+    "DateUtc": "2026-04-18T10:00:00Z",
+    "RoundNumber": 26,
+    "HomeTeam": "Royal Challengers Bengaluru",
+    "AwayTeam": "Delhi Capitals",
+    "Location": "Bengaluru"
+  },
+  {
+    "MatchNumber": 27,
+    "DateUtc": "2026-04-18T14:00:00Z",
+    "RoundNumber": 27,
+    "HomeTeam": "Sunrisers Hyderabad",
+    "AwayTeam": "Chennai Super Kings",
+    "Location": "Hyderabad"
+  },
+  {
+    "MatchNumber": 28,
+    "DateUtc": "2026-04-19T10:00:00Z",
+    "RoundNumber": 28,
+    "HomeTeam": "Kolkata Knight Riders",
+    "AwayTeam": "Rajasthan Royals",
+    "Location": "Kolkata"
+  },
+  {
+    "MatchNumber": 29,
+    "DateUtc": "2026-04-19T14:00:00Z",
+    "RoundNumber": 29,
+    "HomeTeam": "Punjab Kings",
+    "AwayTeam": "Lucknow Super Giants",
+    "Location": "New Chandigarh"
+  },
+  {
+    "MatchNumber": 30,
+    "DateUtc": "2026-04-20T14:00:00Z",
+    "RoundNumber": 30,
+    "HomeTeam": "Gujarat Titans",
+    "AwayTeam": "Mumbai Indians",
+    "Location": "Ahmedabad"
+  },
+  {
+    "MatchNumber": 31,
+    "DateUtc": "2026-04-21T14:00:00Z",
+    "RoundNumber": 31,
+    "HomeTeam": "Sunrisers Hyderabad",
+    "AwayTeam": "Delhi Capitals",
+    "Location": "Hyderabad"
+  },
+  {
+    "MatchNumber": 32,
+    "DateUtc": "2026-04-22T14:00:00Z",
+    "RoundNumber": 32,
+    "HomeTeam": "Lucknow Super Giants",
+    "AwayTeam": "Rajasthan Royals",
+    "Location": "Lucknow"
+  },
+  {
+    "MatchNumber": 33,
+    "DateUtc": "2026-04-23T14:00:00Z",
+    "RoundNumber": 33,
+    "HomeTeam": "Mumbai Indians",
+    "AwayTeam": "Chennai Super Kings",
+    "Location": "Mumbai"
+  },
+  {
+    "MatchNumber": 34,
+    "DateUtc": "2026-04-24T14:00:00Z",
+    "RoundNumber": 34,
+    "HomeTeam": "Royal Challengers Bengaluru",
+    "AwayTeam": "Gujarat Titans",
+    "Location": "Bengaluru"
+  },
+  {
+    "MatchNumber": 35,
+    "DateUtc": "2026-04-25T10:00:00Z",
+    "RoundNumber": 35,
+    "HomeTeam": "Delhi Capitals",
+    "AwayTeam": "Punjab Kings",
+    "Location": "Delhi"
+  },
+  {
+    "MatchNumber": 36,
+    "DateUtc": "2026-04-25T14:00:00Z",
+    "RoundNumber": 36,
+    "HomeTeam": "Rajasthan Royals",
+    "AwayTeam": "Sunrisers Hyderabad",
+    "Location": "Jaipur"
+  },
+  {
+    "MatchNumber": 37,
+    "DateUtc": "2026-04-26T10:00:00Z",
+    "RoundNumber": 37,
+    "HomeTeam": "Gujarat Titans",
+    "AwayTeam": "Chennai Super Kings",
+    "Location": "Ahmedabad"
+  },
+  {
+    "MatchNumber": 38,
+    "DateUtc": "2026-04-26T14:00:00Z",
+    "RoundNumber": 38,
+    "HomeTeam": "Lucknow Super Giants",
+    "AwayTeam": "Kolkata Knight Riders",
+    "Location": "Lucknow"
+  },
+  {
+    "MatchNumber": 39,
+    "DateUtc": "2026-04-27T14:00:00Z",
+    "RoundNumber": 39,
+    "HomeTeam": "Delhi Capitals",
+    "AwayTeam": "Royal Challengers Bengaluru",
+    "Location": "Delhi"
+  },
+  {
+    "MatchNumber": 40,
+    "DateUtc": "2026-04-28T14:00:00Z",
+    "RoundNumber": 40,
+    "HomeTeam": "Punjab Kings",
+    "AwayTeam": "Rajasthan Royals",
+    "Location": "New Chandigarh"
+  },
+  {
+    "MatchNumber": 41,
+    "DateUtc": "2026-04-29T14:00:00Z",
+    "RoundNumber": 41,
+    "HomeTeam": "Mumbai Indians",
+    "AwayTeam": "Sunrisers Hyderabad",
+    "Location": "Mumbai"
+  },
+  {
+    "MatchNumber": 42,
+    "DateUtc": "2026-04-30T14:00:00Z",
+    "RoundNumber": 42,
+    "HomeTeam": "Gujarat Titans",
+    "AwayTeam": "Royal Challengers Bengaluru",
+    "Location": "Ahmedabad"
+  },
+  {
+    "MatchNumber": 43,
+    "DateUtc": "2026-05-01T14:00:00Z",
+    "RoundNumber": 43,
+    "HomeTeam": "Rajasthan Royals",
+    "AwayTeam": "Delhi Capitals",
+    "Location": "Jaipur"
+  },
+  {
+    "MatchNumber": 44,
+    "DateUtc": "2026-05-02T14:00:00Z",
+    "RoundNumber": 44,
+    "HomeTeam": "Chennai Super Kings",
+    "AwayTeam": "Mumbai Indians",
+    "Location": "Chennai"
+  },
+  {
+    "MatchNumber": 45,
+    "DateUtc": "2026-05-03T10:00:00Z",
+    "RoundNumber": 45,
+    "HomeTeam": "Sunrisers Hyderabad",
+    "AwayTeam": "Kolkata Knight Riders",
+    "Location": "Hyderabad"
+  },
+  {
+    "MatchNumber": 46,
+    "DateUtc": "2026-05-03T14:00:00Z",
+    "RoundNumber": 46,
+    "HomeTeam": "Gujarat Titans",
+    "AwayTeam": "Punjab Kings",
+    "Location": "Ahmedabad"
+  },
+  {
+    "MatchNumber": 47,
+    "DateUtc": "2026-05-04T14:00:00Z",
+    "RoundNumber": 47,
+    "HomeTeam": "Mumbai Indians",
+    "AwayTeam": "Lucknow Super Giants",
+    "Location": "Mumbai"
+  },
+  {
+    "MatchNumber": 48,
+    "DateUtc": "2026-05-05T14:00:00Z",
+    "RoundNumber": 48,
+    "HomeTeam": "Delhi Capitals",
+    "AwayTeam": "Chennai Super Kings",
+    "Location": "Delhi"
+  },
+  {
+    "MatchNumber": 49,
+    "DateUtc": "2026-05-06T14:00:00Z",
+    "RoundNumber": 49,
+    "HomeTeam": "Sunrisers Hyderabad",
+    "AwayTeam": "Punjab Kings",
+    "Location": "Hyderabad"
+  },
+  {
+    "MatchNumber": 50,
+    "DateUtc": "2026-05-07T14:00:00Z",
+    "RoundNumber": 50,
+    "HomeTeam": "Lucknow Super Giants",
+    "AwayTeam": "Royal Challengers Bengaluru",
+    "Location": "Lucknow"
+  },
+  {
+    "MatchNumber": 51,
+    "DateUtc": "2026-05-08T14:00:00Z",
+    "RoundNumber": 51,
+    "HomeTeam": "Delhi Capitals",
+    "AwayTeam": "Kolkata Knight Riders",
+    "Location": "Delhi"
+  },
+  {
+    "MatchNumber": 52,
+    "DateUtc": "2026-05-09T14:00:00Z",
+    "RoundNumber": 52,
+    "HomeTeam": "Rajasthan Royals",
+    "AwayTeam": "Gujarat Titans",
+    "Location": "Jaipur"
+  },
+  {
+    "MatchNumber": 53,
+    "DateUtc": "2026-05-10T10:00:00Z",
+    "RoundNumber": 53,
+    "HomeTeam": "Chennai Super Kings",
+    "AwayTeam": "Lucknow Super Giants",
+    "Location": "Chennai"
+  },
+  {
+    "MatchNumber": 54,
+    "DateUtc": "2026-05-10T14:00:00Z",
+    "RoundNumber": 54,
+    "HomeTeam": "Royal Challengers Bengaluru",
+    "AwayTeam": "Mumbai Indians",
+    "Location": "Raipur"
+  },
+  {
+    "MatchNumber": 55,
+    "DateUtc": "2026-05-11T14:00:00Z",
+    "RoundNumber": 55,
+    "HomeTeam": "Punjab Kings",
+    "AwayTeam": "Delhi Capitals",
+    "Location": "Dharamshala"
+  },
+  {
+    "MatchNumber": 56,
+    "DateUtc": "2026-05-12T14:00:00Z",
+    "RoundNumber": 56,
+    "HomeTeam": "Gujarat Titans",
+    "AwayTeam": "Sunrisers Hyderabad",
+    "Location": "Ahmedabad"
+  },
+  {
+    "MatchNumber": 57,
+    "DateUtc": "2026-05-13T14:00:00Z",
+    "RoundNumber": 57,
+    "HomeTeam": "Royal Challengers Bengaluru",
+    "AwayTeam": "Kolkata Knight Riders",
+    "Location": "Raipur"
+  },
+  {
+    "MatchNumber": 58,
+    "DateUtc": "2026-05-14T14:00:00Z",
+    "RoundNumber": 58,
+    "HomeTeam": "Punjab Kings",
+    "AwayTeam": "Mumbai Indians",
+    "Location": "Dharamshala"
+  },
+  {
+    "MatchNumber": 59,
+    "DateUtc": "2026-05-15T14:00:00Z",
+    "RoundNumber": 59,
+    "HomeTeam": "Lucknow Super Giants",
+    "AwayTeam": "Chennai Super Kings",
+    "Location": "Lucknow"
+  },
+  {
+    "MatchNumber": 60,
+    "DateUtc": "2026-05-16T14:00:00Z",
+    "RoundNumber": 60,
+    "HomeTeam": "Kolkata Knight Riders",
+    "AwayTeam": "Gujarat Titans",
+    "Location": "Kolkata"
+  },
+  {
+    "MatchNumber": 61,
+    "DateUtc": "2026-05-17T10:00:00Z",
+    "RoundNumber": 61,
+    "HomeTeam": "Punjab Kings",
+    "AwayTeam": "Royal Challengers Bengaluru",
+    "Location": "Dharamshala"
+  },
+  {
+    "MatchNumber": 62,
+    "DateUtc": "2026-05-17T14:00:00Z",
+    "RoundNumber": 62,
+    "HomeTeam": "Delhi Capitals",
+    "AwayTeam": "Rajasthan Royals",
+    "Location": "Delhi"
+  },
+  {
+    "MatchNumber": 63,
+    "DateUtc": "2026-05-18T14:00:00Z",
+    "RoundNumber": 63,
+    "HomeTeam": "Chennai Super Kings",
+    "AwayTeam": "Sunrisers Hyderabad",
+    "Location": "Chennai"
+  },
+  {
+    "MatchNumber": 64,
+    "DateUtc": "2026-05-19T14:00:00Z",
+    "RoundNumber": 64,
+    "HomeTeam": "Rajasthan Royals",
+    "AwayTeam": "Lucknow Super Giants",
+    "Location": "Jaipur"
+  },
+  {
+    "MatchNumber": 65,
+    "DateUtc": "2026-05-20T14:00:00Z",
+    "RoundNumber": 65,
+    "HomeTeam": "Kolkata Knight Riders",
+    "AwayTeam": "Mumbai Indians",
+    "Location": "Kolkata"
+  },
+  {
+    "MatchNumber": 66,
+    "DateUtc": "2026-05-21T14:00:00Z",
+    "RoundNumber": 66,
+    "HomeTeam": "Chennai Super Kings",
+    "AwayTeam": "Gujarat Titans",
+    "Location": "Chennai"
+  },
+  {
+    "MatchNumber": 67,
+    "DateUtc": "2026-05-22T14:00:00Z",
+    "RoundNumber": 67,
+    "HomeTeam": "Sunrisers Hyderabad",
+    "AwayTeam": "Royal Challengers Bengaluru",
+    "Location": "Hyderabad"
+  },
+  {
+    "MatchNumber": 68,
+    "DateUtc": "2026-05-23T14:00:00Z",
+    "RoundNumber": 68,
+    "HomeTeam": "Lucknow Super Giants",
+    "AwayTeam": "Punjab Kings",
+    "Location": "Lucknow"
+  },
+  {
+    "MatchNumber": 69,
+    "DateUtc": "2026-05-24T10:00:00Z",
+    "RoundNumber": 69,
+    "HomeTeam": "Mumbai Indians",
+    "AwayTeam": "Rajasthan Royals",
+    "Location": "Mumbai"
+  },
+  {
+    "MatchNumber": 70,
+    "DateUtc": "2026-05-24T14:00:00Z",
+    "RoundNumber": 70,
+    "HomeTeam": "Kolkata Knight Riders",
+    "AwayTeam": "Delhi Capitals",
+    "Location": "Kolkata"
   }
 ];
 
